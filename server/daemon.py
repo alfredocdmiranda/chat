6b9fe281ac83d080015b9aba30cd616c46416b4a
Alfredo Miranda <alfredo@ada-one>
2015-07-09 15:15:52 -0300

    Changed the way that read the arguments.

--- server/daemon.py
+++ server/daemon.py
@@ -129,23 +129,3 @@ class Daemon(object):
         daemonized by start() or restart().
         """
         pass
-        #while 1:
-        #    print("RUN")
-        #    time.sleep(1)
-
-if __name__ == "__main__":
-    daemon = Daemon('/var/run/pirc-server.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print("usage: {0} start|stop|restart".format(sys.argv[0]))
-        sys.exit(2)
